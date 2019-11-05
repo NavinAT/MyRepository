48f9b1316e8be0265d21f38fc750eb17b07ec9e5
naveen <naveen@MS-00603>
2019-11-05 19:02:30 +0530

    ADDED: Employee Service WEB API

---
+++ EmployeeService/EmployeeService/App_Start/WebApiConfig.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Http;
+using Microsoft.Owin.Security.OAuth;
+
+namespace EmployeeService
+{
+	public static class WebApiConfig
+	{
+		#region Publics
+		public static void Register(HttpConfiguration config)
+		{
+			// Web API configuration and services
+			// Configure Web API to use only bearer token authentication.
+			config.SuppressDefaultHostAuthentication();
+			config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+
+			// Web API routes
+			config.MapHttpAttributeRoutes();
+
+			config.Routes.MapHttpRoute(
+			                           name: "DefaultApi",
+			                           routeTemplate: "api/Employee/{controller}/{id}",
+			                           defaults: new { id = RouteParameter.Optional }
+			                          );
+		}
+		#endregion
+	}
+}
---
+++ EmployeeService/EmployeeService/Controllers/EmployeesController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using EmployeeService.Models;
+
+namespace EmployeeService.Controllers
+{
+	public class EmployeesController : ApiController
+	{
+		#region Publics
+		public IEnumerable<Employee> GetEmployees()
+		{
+			using(EmployeeDBContext employeeDb = new EmployeeDBContext())
+			{
+				return employeeDb.Employees.ToList();
+			}
+		}
+
+		public Employee GetEmployee(int nId)
+		{
+			using(EmployeeDBContext employeeDb = new EmployeeDBContext())
+			{
+				return employeeDb.Employees.FirstOrDefault(row => row.ID == nId);
+			}
+		}
+		#endregion
+	}
+}
