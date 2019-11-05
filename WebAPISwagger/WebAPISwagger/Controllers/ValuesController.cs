9109e8a34067ef7ae2c4e96a96c0fc89fc40bc54
naveen <naveen@MS-00603>
2019-11-05 15:02:29 +0530

    R#

--- WebAPISwagger/WebAPISwagger/Controllers/ValuesController.cs
+++ WebAPISwagger/WebAPISwagger/Controllers/ValuesController.cs
@@ -1,40 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using System.Collections.Generic;
 using System.Web.Http;
 
 namespace WebAPISwagger.Controllers
 {
-    //[Authorize]
-    public class ValuesController : ApiController
-    {
-        // GET api/values
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+	//[Authorize]
+	public class ValuesController : ApiController
+	{
+		#region Publics
+		// GET api/values
+		public IEnumerable<string> Get()
+		{
+			return new[] { "value1", "value2" };
+		}
 
-        // GET api/values/5
-        public string Get(int id)
-        {
-            return "value";
-        }
+		// GET api/values/5
+		public string Get(int id)
+		{
+			return "value";
+		}
 
-        // POST api/values
-        public void Post([FromBody]string value)
-        {
-        }
+		// POST api/values
+		public void Post([FromBody] string value)
+		{
+		}
 
-        // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
+		// PUT api/values/5
+		public void Put(int id, [FromBody] string value)
+		{
+		}
 
-        // DELETE api/values/5
-        public void Delete(int id)
-        {
-        }
-    }
+		// DELETE api/values/5
+		public void Delete(int id)
+		{
+		}
+		#endregion
+	}
 }
