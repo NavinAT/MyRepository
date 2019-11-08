7c319e71148b9c4a8104ee4f53bd82ad54989957
naveen <naveen@MS-00603>
2019-11-08 12:04:35 +0530

    CHANGED: Added Media formatter

--- EmployeeService/EmployeeService/App_Start/WebApiConfig.cs
+++ EmployeeService/EmployeeService/App_Start/WebApiConfig.cs
@@ -1,5 +1,11 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Web.Http;
+using System.Web.UI.WebControls;
+using Microsoft.Ajax.Utilities;
 using Microsoft.Owin.Security.OAuth;
+using RouteParameter = System.Web.Http.RouteParameter;
 
 namespace EmployeeService
 {
@@ -12,6 +18,7 @@ namespace EmployeeService
 			// Configure Web API to use only bearer token authentication.
 			config.SuppressDefaultHostAuthentication();
 			config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+			config.Formatters.Add(new CustomJsonFormatter());
 
 			// Web API routes
 			config.MapHttpAttributeRoutes();
@@ -21,6 +28,20 @@ namespace EmployeeService
 			                           routeTemplate: "api/Employee/{controller}/{id}",
 			                           defaults: new { id = RouteParameter.Optional }
 			                          );
+		}
+
+		private class CustomJsonFormatter : JsonMediaTypeFormatter
+		{
+			public CustomJsonFormatter()
+			{
+				this.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+			}
+
+			public override void SetDefaultContentHeaders(Type type, HttpContentHeaders headers, MediaTypeHeaderValue mediaType)
+			{
+				base.SetDefaultContentHeaders(type, headers, mediaType);
+				headers.ContentType = new MediaTypeHeaderValue("application/json");
+			}
 		}
 		#endregion
 	}
