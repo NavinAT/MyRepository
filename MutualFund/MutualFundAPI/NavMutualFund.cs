08646f96736a8a7314a2fd777262834702962778
naveen <naveen@MS-00603>
2019-11-08 12:13:23 +0530

    ADDED: Lufthansa API call

---
+++ MutualFund/MutualFundAPI/NavMutualFund.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace MutualFundAPI
+{
+	public class NavMutualFund
+	{
+		#region Publics
+		public static async Task<ResponseFromLufthansa> GetAccessToken(LufthansaAccessToken searchCriteria)
+		{
+			ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+
+			HttpClient client = new HttpClient { BaseAddress = new Uri("https://api.lufthansa.com") };
+			HttpRequestMessage httpRequest = new HttpRequestMessage(HttpMethod.Post, "/v1/oauth/token");
+
+			string strrequestContent =
+				$"client_id={searchCriteria.ClientId}&client_secret={searchCriteria.ClientSecret}&grant_type={searchCriteria.GrantType}";
+
+			StringContent _content = new StringContent(strrequestContent);
+			_content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/x-www-form-urlencoded");
+			httpRequest.Content = _content;
+
+			HttpResponseMessage httpResponse = await client.SendAsync(httpRequest).ConfigureAwait(false);
+
+			string _responsedata = httpResponse.Content == null ? null : await httpResponse.Content.ReadAsStringAsync().ConfigureAwait(false);
+			ResponseFromLufthansa _result = JsonConvert.DeserializeObject<ResponseFromLufthansa>(_responsedata);
+
+			return _result;
+		}
+		#endregion
+
+		#region Privates
+		private static void Main(string[] args)
+		{
+		}
+		#endregion
+	}
+
+	public class LufthansaAccessToken
+	{
+		#region Properties
+		public string ClientId { get; set; }
+		public string ClientSecret { get; set; }
+		public string GrantType { get; set; }
+		#endregion
+	}
+
+	public class ResponseFromLufthansa
+	{
+		#region Properties
+		[JsonProperty("access_token", Required = Required.Default, NullValueHandling = NullValueHandling.Ignore)]
+		public string AccessToken { get; set; }
+
+		[JsonProperty("token_type", Required = Required.Default, NullValueHandling = NullValueHandling.Ignore)]
+		public string TokenType { get; set; }
+		#endregion
+	}
+}
---
+++ MutualFund/MutualFundAPITest/NavMutualFundTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MutualFundAPI;
+
+namespace MutualFundAPITest
+{
+	[TestClass]
+	public class NavMutualFundTest
+	{
+		#region Tests
+		[TestMethod]
+		public void TestGetMutualFundNav()
+		{
+			// Arrange
+			LufthansaAccessToken accessToken = new LufthansaAccessToken
+			                                   {
+				                                   ClientId = "r2gzamvxt7x24c595vb5ww24",
+				                                   ClientSecret = "rGJqxEZCW4",
+				                                   GrantType = "client_credentials"
+			                                   };
+
+			// Act
+			ResponseFromLufthansa responseFromLufthansa = NavMutualFund.GetAccessToken(accessToken).GetAwaiter().GetResult();
+
+			// Assert
+		}
+		#endregion
+	}
+}
